0d3430f43865d60d861bb512493027ef4b58a29f
Zakarya <zakaryacarroll.contact@gmail.com>
2023-12-01 22:06:46 -0800

    Fix the bug where the movement would not follow the camera

--- data/player/player.gd
+++ data/player/player.gd
@@ -39,91 +39,91 @@ var crouch_player_y_scale: float = 0.75
 
 # Node References
 @onready var parts: Dictionary = {
-    "head": $head,
-    "camera": $head/camera,
-    "camera_animation": $head/camera/camera_animation,
-    "body": $body,
-    "collision": $collision
+	"head": $head,
+	"camera": $head/camera,
+	"camera_animation": $head/camera/camera_animation,
+	"body": $body,
+	"collision": $collision
 }
 @onready var world: SceneTree = get_tree()
 
 func _ready() -> void:
-    parts["camera"].current = true
+	parts["camera"].current = true
 
 func _process(delta: float) -> void:
-    handle_movement_input(delta)
-    update_camera(delta)
+	handle_movement_input(delta)
+	update_camera(delta)
 
 func _physics_process(delta: float) -> void:
-    apply_gravity(delta)
-    handle_jump()
-    move_character(delta)
+	apply_gravity(delta)
+	handle_jump()
+	move_character(delta)
 
 func _input(event: InputEvent) -> void:
-    if event is InputEventMouseMotion:
-        handle_mouse_movement(event)
+	if event is InputEventMouseMotion:
+		handle_mouse_movement(event)
 
 # Movement Logic
 func handle_movement_input(delta: float) -> void:
-    if Input.is_action_pressed("move_sprint") and !Input.is_action_pressed("move_crouch") and sprint_enabled:
-        enter_sprint_state(delta)
-    elif Input.is_action_pressed("move_crouch") and !Input.is_action_pressed("move_sprint") and sprint_enabled:
-        enter_crouch_state(delta)
-    else:
-        enter_normal_state(delta)
+	if Input.is_action_pressed("move_sprint") and !Input.is_action_pressed("move_crouch") and sprint_enabled:
+		enter_sprint_state(delta)
+	elif Input.is_action_pressed("move_crouch") and !Input.is_action_pressed("move_sprint") and sprint_enabled:
+		enter_crouch_state(delta)
+	else:
+		enter_normal_state(delta)
 
 func enter_sprint_state(delta: float) -> void:
-    state = "sprinting"
-    speed = sprint_speed
-    parts["camera"].fov = lerp(parts["camera"].fov, camera_fov_extents[1], 10 * delta)
+	state = "sprinting"
+	speed = sprint_speed
+	parts["camera"].fov = lerp(parts["camera"].fov, camera_fov_extents[1], 10 * delta)
 
 func enter_crouch_state(delta: float) -> void:
-    state = "crouching"
-    speed = crouch_speed
-    apply_crouch_transform(delta)
+	state = "crouching"
+	speed = crouch_speed
+	apply_crouch_transform(delta)
 
 func enter_normal_state(delta: float) -> void:
-    state = "normal"
-    speed = base_speed
-    reset_transforms(delta)
+	state = "normal"
+	speed = base_speed
+	reset_transforms(delta)
 
 # Camera Logic
 func update_camera(delta: float) -> void:
-    match state:
-        "sprinting":
-            parts["camera"].fov = lerp(parts["camera"].fov, camera_fov_extents[1], 10 * delta)
-        "normal":
-            parts["camera"].fov = lerp(parts["camera"].fov, camera_fov_extents[0], 10 * delta)
+	match state:
+		"sprinting":
+			parts["camera"].fov = lerp(parts["camera"].fov, camera_fov_extents[1], 10 * delta)
+		"normal":
+			parts["camera"].fov = lerp(parts["camera"].fov, camera_fov_extents[0], 10 * delta)
 
 # Animation Logic
 func apply_crouch_transform(delta: float) -> void:
-    parts["body"].scale.y = lerp(parts["body"].scale.y, crouch_player_y_scale, 10 * delta)
-    parts["collision"].scale.y = lerp(parts["collision"].scale.y, crouch_player_y_scale, 10 * delta)
+	parts["body"].scale.y = lerp(parts["body"].scale.y, crouch_player_y_scale, 10 * delta)
+	parts["collision"].scale.y = lerp(parts["collision"].scale.y, crouch_player_y_scale, 10 * delta)
 
 func reset_transforms(delta: float) -> void:
-    parts["body"].scale.y = lerp(parts["body"].scale.y, base_player_y_scale, 10 * delta)
-    parts["collision"].scale.y = lerp(parts["collision"].scale.y, base_player_y_scale, 10 * delta)
+	parts["body"].scale.y = lerp(parts["body"].scale.y, base_player_y_scale, 10 * delta)
+	parts["collision"].scale.y = lerp(parts["collision"].scale.y, base_player_y_scale, 10 * delta)
 
 # Physics Logic
 func apply_gravity(delta: float) -> void:
-    if not is_on_floor():
-        velocity.y -= gravity * delta
+	if not is_on_floor():
+		velocity.y -= gravity * delta
 
 func handle_jump() -> void:
-    if Input.is_action_pressed("move_jump") and is_on_floor():
-        velocity.y += jump_velocity
+	if Input.is_action_pressed("move_jump") and is_on_floor():
+		velocity.y += jump_velocity
 
 func move_character(delta: float) -> void:
-    var input_dir: Vector2 = Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
-    var direction: Vector3 = Vector3(input_dir.x, 0, input_dir.y)
-    if is_on_floor():
-        velocity.x = lerp(velocity.x, direction.x * speed, accel * delta)
-        velocity.z = lerp(velocity.z, direction.z * speed, accel * delta)
-    move_and_slide()
+	var input_dir: Vector2 = Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
+	var direction: Vector2 = input_dir.normalized().rotated(-parts["head"].rotation.y)
+	if is_on_floor():
+		velocity.x = lerp(velocity.x, direction.x * speed, accel * delta)
+		velocity.z = lerp(velocity.z, direction.y * speed, accel * delta)
+	move_and_slide()
 
 # Input Handling
 func handle_mouse_movement(event: InputEventMouseMotion) -> void:
-    if !world.paused:
-        parts["head"].rotation_degrees.y -= event.relative.x * sensitivity
-        parts["head"].rotation_degrees.x -= event.relative.y * sensitivity
-        parts["head"].rotation.x = clamp(parts["head"].rotation.x, deg_to_rad(-90), deg_to_rad(90))
+	if !world.paused:
+		parts["head"].rotation_degrees.y -= event.relative.x * sensitivity
+		parts["head"].rotation_degrees.x -= event.relative.y * sensitivity
+		parts["head"].rotation.x = clamp(parts["head"].rotation.x, deg_to_rad(-90), deg_to_rad(90))
